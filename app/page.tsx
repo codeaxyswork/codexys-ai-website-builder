63b7d8e7bd42487693099c79db89a3386579f991
codeaxyswork <sujithcjohn@gmail.com>
2026-09-10 21:29:44 +0530

    Disable stale Vercel CDN static caching on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import React, { useState, useEffect } from "react";
 import { Header } from "@/components/Header";
 import { LandingView } from "@/components/LandingView";
